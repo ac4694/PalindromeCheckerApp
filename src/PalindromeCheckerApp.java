72c331efe1003c13e832e1b9520c90224daf6e19
Adharsh <addylakers24@gmail.com>
2026-03-02 14:29:01 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class PalindromeCheckerApp {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+  // UC2: Hardcoded Palindrome Check
+  public static void main(String[] args) {
+
+    String input = "madam";   // Hardcoded string
+    boolean isPalindrome = true;
+
+    int left = 0;
+    int right = input.length() - 1;
+
+    while (left < right) {
+      if (input.charAt(left) != input.charAt(right)) {
+        isPalindrome = false;
+        break;
+      }
+      left++;
+      right--;
+    }
+
+    System.out.println("Hardcoded String : " + input);
+
+    if (isPalindrome) {
+      System.out.println("Result : Palindrome");
+    } else {
+      System.out.println("Result : Not a Palindrome");
+    }
   }
 }
