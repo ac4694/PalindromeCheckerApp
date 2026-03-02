e496371f20a9011565cdc4c9c8aaa51a468aaf20
Adharsh <addylakers24@gmail.com>
2026-03-02 14:57:20 +0530

    UC5: Palindrome check using stack (LIFO)

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Stack;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+public class PalindromeCheckerApp {
+
+
+  public static void main(String[] args) {
+
+    String input = "madam";   // Hardcoded string
+    Stack<Character> stack = new Stack<>();
+
+    // Push characters into stack
+    for (int i = 0; i < input.length(); i++) {
+      stack.push(input.charAt(i));
+    }
+
+    // Pop characters to build reversed string
+    String reversed = "";
+    while (!stack.isEmpty()) {
+      reversed = reversed + stack.pop();
+    }
+
+    System.out.println("Input String : " + input);
+    System.out.println("Reversed String : " + reversed);
+
+    // Compare original and reversed
+    if (input.equals(reversed)) {
+      System.out.println("Result : Palindrome");
+    } else {
+      System.out.println("Result : Not a Palindrome");
+    }
   }
 }
