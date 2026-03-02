8a43ec54f1464787562447daad8e34c0cb2e79e9
Adharsh <addylakers24@gmail.com>
2026-03-02 14:38:23 +0530

    UC3: Palindrome check using string reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class PalindromeCheckerApp {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+  // UC3: Palindrome Check Using String Reverse
+  public static void main(String[] args) {
+
+    String original = "madam";   // Hardcoded string
+    String reversed = "";
+
+    // Reverse the string using for loop
+    for (int i = original.length() - 1; i >= 0; i--) {
+      reversed = reversed + original.charAt(i);
+    }
+
+    System.out.println("Original String : " + original);
+    System.out.println("Reversed String : " + reversed);
+
+    // Compare original and reversed string
+    if (original.equals(reversed)) {
+      System.out.println("Result : Palindrome");
+    } else {
+      System.out.println("Result : Not a Palindrome");
+    }
   }
 }
