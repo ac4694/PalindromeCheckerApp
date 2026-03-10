23b4d6ae499de61ccd2b909cb55af7897b57df80
Adharsh <addylakers24@gmail.com>
2026-03-10 13:53:05 +0530

    UC7: Palindrome check using Deque (front and rear comparison)

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Deque;
+import java.util.LinkedList;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+public class PalindromeCheckerApp {
+
+  // UC7: Deque-Based Optimized Palindrome Checker
+  public static void main(String[] args) {
+
+    String input = "madam";   // Hardcoded string
+
+    Deque<Character> deque = new LinkedList<>();
+
+    // Insert characters into deque
+    for (int i = 0; i < input.length(); i++) {
+      deque.addLast(input.charAt(i));
+    }
+
+    boolean isPalindrome = true;
+
+    // Compare front and rear characters
+    while (deque.size() > 1) {
+
+      char front = deque.removeFirst();
+      char rear = deque.removeLast();
+
+      if (front != rear) {
+        isPalindrome = false;
+        break;
+      }
+    }
+
+    System.out.println("Input String : " + input);
+
+    if (isPalindrome) {
+      System.out.println("Result : Palindrome");
+    } else {
+      System.out.println("Result : Not a Palindrome");
+    }
   }
 }
