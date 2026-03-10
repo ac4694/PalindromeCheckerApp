dfdfa6aa25790a612e7a51dc095050610489ed35
Adharsh <addylakers24@gmail.com>
2026-03-10 14:16:32 +0530

    UC9: Palindrome check using recursion

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,14 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class PalindromeCheckerApp {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+  // Recursive method to check palindrome
+  public static boolean isPalindrome(String str, int left, int right) {
+
+    // Base condition
+    if (left >= right) {
+      return true;
+    }
+
+    // If characters don't match
+    if (str.charAt(left) != str.charAt(right)) {
+      return false;
+    }
+
+    // Recursive call
+    return isPalindrome(str, left + 1, right - 1);
+  }
+
+  public static void main(String[] args) {
+
+    String input = "madam";
+
+    boolean result = isPalindrome(input, 0, input.length() - 1);
+
+    System.out.println("Input String : " + input);
+
+    if (result) {
+      System.out.println("Result : Palindrome");
+    } else {
+      System.out.println("Result : Not a Palindrome");
+    }
   }
 }
-
