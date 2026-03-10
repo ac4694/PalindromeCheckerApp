c6d30a49a16f4d4545f8dca84956eec608bf97ef
Adharsh <addylakers24@gmail.com>
2026-03-10 14:18:44 +0530

    UC10: Case-insensitive and space-ignored palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,13 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class PalindromeCheckerApp {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
+  public static void main(String[] args) {
+
+    String input = "Madam In Eden Im Adam";
+
+    // Normalize the string
+    String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
+    int left = 0;
+    int right = normalized.length() - 1;
+    boolean isPalindrome = true;
+
+    while (left < right) {
+      if (normalized.charAt(left) != normalized.charAt(right)) {
+        isPalindrome = false;
+        break;
+      }
+      left++;
+      right--;
+    }
+
+    System.out.println("Original String : " + input);
+
+    if (isPalindrome) {
+      System.out.println("Result : Palindrome (Ignoring spaces and case)");
+    } else {
+      System.out.println("Result : Not a Palindrome");
+    }
   }
 }
